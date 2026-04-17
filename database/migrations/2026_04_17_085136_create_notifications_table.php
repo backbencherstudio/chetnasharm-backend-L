9a3e0dce612425cd2f2629eb5b69242a39482edb
Shahadat-Hossain-Shanto <shahadathossain1563@gmail.com>
2026-04-17 14:57:55 +0600

    notification log tables

---
+++ database/migrations/2026_04_17_085136_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('batch_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->enum('type', ['email', 'whatsapp']);
+            $table->string('message_type')->nullable();
+            $table->text('message');
+
+            $table->enum('status', ['sent', 'failed'])->default('sent');
+            $table->timestamp('sent_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
