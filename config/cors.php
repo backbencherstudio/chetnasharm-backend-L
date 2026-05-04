2c3ec011a6d927a37877ad908665ac62d0b262c5
Shahadat-Hossain-Shanto <shahadathossain1563@gmail.com>
2026-05-04 09:52:56 +0600

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,50 @@
 <?php
+//
+//return [
+//    'paths' => ['api/*','*'],
+//
+//    'allowed_methods' => ['*'],
+//
+//    'allowed_origins' => ['*', 'https://listenact.vercel.app'],
+//
+//    'allowed_origins_patterns' => [],
+//
+//    'allowed_headers' => ['*'],
+//
+//    'exposed_headers' => [],
+//
+//    'max_age' => 0,
+//
+//    'supports_credentials' => false,
+//
+//];
 
 return [
-    'paths' => ['api/*','*'],
+
+    'paths' => [
+        'api/*',
+    ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env(
+        'CORS_ALLOWED_ORIGINS'
+    )),
 
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
+
+    // 'allowed_headers' => [
+    //     'Content-Type',
+    //     'X-Requested-With',
+    //     'Authorization',
+    //     'Accept',
+    //     'Origin',
+    // ],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => false,
 
